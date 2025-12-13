9a490dff8090d479e5f4dc89979004275d092151
H <h@Hs-MacBook-Air.local>
2025-12-13 15:20:22 +0300

    Initial commit - pastry shop website

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/pastry-shop',
-  assetPrefix: '/pastry-shop/', // required for static export on GitHub Pages
-
-  images: {
-    unoptimized: true,
-  },
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
